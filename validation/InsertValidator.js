911e0632d4367e38188faa5c52c3aa289efa386d
Ben Botto <benjamin.botto@gmail.com>
2016-10-15 10:03:47 -0700

    InsertValidator updated to use new per-column validation logic (cleanup).

--- validation/InsertValidator.js
+++ validation/InsertValidator.js
@@ -14,21 +14,29 @@ require('insulin').factory('InsertValidator', function(ModelValidator) {
      */
     constructor(model, tableAlias, database) {
       super(model, tableAlias, database);
+    }
 
-      const table   = database.getTableByAlias(tableAlias);
-      const pkAlias = table.getPrimaryKey()[0].getAlias();
+    /**
+     * Override the validation initialization and ensure that each non-nullable
+     * field is defined.  The exception is is if there is a default value, or
+     * if the column is a primary key.
+     * @param col The database Column instance for which validation constraints
+     *        should be initialized.
+     */
+    setColumnValidation(col) {
+      // This calls this.key(col.getAlias()) so the key is set.
+      super.setColumnValidation(col);
+
+      // If the column isn't nullalbe, it generally must be defined.  The
+      // exceptions to the rule are: 1) The primary key, or 2) columns with
+      // default values.  Note, however, that non-nullable fields can never
+      // be null, which is enforced by ModelValidator.
+      if (!col.isNullable() && !col.isPrimary() && !col.getDefaultValue())
+        this.defined();
 
       // Primary key is not allowed on insert.
-      this.key(pkAlias).notDefined();
-
-      table.getColumns().forEach(col => {
-        // If the column isn't nullalbe, it generally must be defined.  The
-        // exceptions to the rule are: 1) The primary key, or 2) columns with
-        // default values.  Note, however, that non-nullable fields can never
-        // be null, which is enforced by ModelValidator.
-        if (!col.isNullable() && !col.isPrimary() && !col.getDefaultValue())
-          this.key(col.getAlias()).defined();
-      });
+      if (col.isPrimary())
+        this.notDefined();
     }
   }
 
