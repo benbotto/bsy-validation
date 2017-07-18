359de61abcd53ed1295bb456cebb007a43c62a93
benbotto <benbotto@users.noreply.github.com>
2017-07-18 15:13:21 -0700

    basename used on exported files (windows fix).

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const insulin = require('insulin');
+const path    = require('path');
 
 // The bootstrap file lets everything register itself with the DiC, insulin,
 // and returns a list if files.  This script goes through the collection of
-// files returned by the bootstrap process and sets up an object that has
-// each error type.  New files are automagically picked up and exported,
-// and consumers can choose whether or not to use insulin.
+// files returned by the bootstrap process and sets up an object that has an
+// instance of each.  New files are automagically picked up and exported, and
+// consumers can choose whether or not to use insulin.
 
-let files = require('./bootstrap');
-let exp   = {};
+const files = require('./bootstrap');
+const exp   = {};
 
 files.forEach(f => {
-  // Remove the everything up to the final "/" and the ".js" extension.
-  let name = f.replace(/^.*\/([^\/]+)\.js$/, '$1');
-  exp[name] = insulin.get(name);
+  // Use the file name, without the extension.
+  const name = path.basename(f, '.js');
+  exp[name]  = insulin.get(name);
 });
 
 module.exports = exp;
