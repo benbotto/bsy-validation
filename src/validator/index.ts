fb2614b30c84607a1d3024e1e3da81f0f220eb7c
Ben Botto <benjamin.botto@gmail.com>
2019-03-13 18:11:38 -0700

    Exports all validators.

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,3 +1,8 @@
 export * from './validator';
 export * from './string-validator';
+export * from './max-length-validator';
+export * from './not-blank-validator';
+export * from './phone-validator';
+
+export * from './object-validator';
 
