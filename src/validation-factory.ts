b0b54c785440ddc6dbce58300383a2473aadb150
Ben Botto <benjamin.botto@gmail.com>
2019-03-13 18:10:08 -0700

    Container for class validation metadata.

---
+++ src/validation-factory.ts
@@ -0,0 +1,32 @@
+import { ValidationMetadata } from './';
+
+class ValidationFactory {
+  private classValidators: Map<{new(): any}, ValidationMetadata[]> = new Map();
+
+  /**
+   * Add a validator to a class.
+   */
+  addMetadata(meta: ValidationMetadata): this {
+    if (!this.classValidators.has(meta.Entity))
+      this.classValidators.set(meta.Entity, []);
+
+    this.classValidators
+      .get(meta.Entity)
+      .push(meta);
+
+    return this;
+  }
+
+  /**
+   * Get the validation metadata for an entity (class).  The returned array of
+   * ValidationMeta will be empty if the class has no constraints in place.
+   */
+  getMetadata(Entity: {new(): any}): ValidationMetadata[] {
+    if (!this.classValidators.has(Entity))
+      return [];
+    return this.classValidators.get(Entity);
+  }
+}
+
+export default new ValidationFactory();
+
