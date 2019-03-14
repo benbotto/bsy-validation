429c33b522cdc6d3613dc0dbaa7a7903a2dbe866
Ben Botto <benjamin.botto@gmail.com>
2019-03-13 18:09:04 -0700

    PhoneValidator.

---
+++ src/validator/phone-validator.spec.ts
@@ -0,0 +1,24 @@
+import { PhoneValidator } from './';
+
+describe('Validator()', () => {
+  const v = new PhoneValidator();
+
+  describe('.validate()', () => {
+    it('returns true if the has at least 10 digits.', () => {
+      expect(v.validate('1234567890')).toBe(true);
+      expect(v.validate('1234567890123')).toBe(true);
+      expect(v.validate('123.456-7890')).toBe(true);
+    });
+
+    it('returns false if the value has fewer than 10 characters.', () => {
+      expect(v.validate('911')).toBe(false);
+    });
+  });
+
+  describe('.getErrorMessage()', () => {
+    it('returns an error message.', () => {
+      expect(v.getErrorMessage('foo')).toBe('"foo" is not a valid phone number.');
+    });
+  });
+});
+
---
+++ src/validator/phone-validator.ts
@@ -0,0 +1,23 @@
+import { Validator, StringValidator } from './';
+
+export class PhoneValidator {
+  /**
+   * This validator only checks that the supplied phone number is a string with
+   * at least 10 digits.  It strips delimiters, and doesn't check a phone
+   * number's validity (e.g. an area code of 099 will pass, but is invalid in
+   * the US).
+   */
+  validate(val: any): boolean {
+    const strVal = new StringValidator();
+
+    if (!strVal.validate(val))
+      return false;
+
+    return val.replace(/\D/g, '').length >= 10;
+  }
+
+  getErrorMessage(propName: string): string {
+    return `"${propName}" is not a valid phone number.`;
+  }
+}
+
