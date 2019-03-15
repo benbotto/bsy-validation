320775098fd6d47f18f3e0add7f1b4ba1938bad5
Ben Botto <benjamin.botto@gmail.com>
2019-03-15 08:40:14 -0700

    Adds support for multiple validators.

--- src/decorator/validation-metadata.ts
+++ src/decorator/validation-metadata.ts
@@ -8,12 +8,13 @@ export class ValidationMetadata {
    * Init.
    * @param Entity - The Entity to validate.
    * @param propName - The property on Entity to validate.
-   * @param validator - The Validator instance to apply to Entity[propName].
+   * @param validators - An array of Validator instances to apply to
+   * Entity[propName].
    */
   constructor(
     public Entity: {new(): any},
     public propName: string,
-    public validator: Validator) {
+    public validators: Validator[]) {
   }
 }
 
