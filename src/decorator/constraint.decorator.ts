6f2b8e44fafae4fe6f6550b8eebe30591a040ead
Ben Botto <benjamin.botto@gmail.com>
2019-03-15 08:40:21 -0700

    Adds support for multiple validators.

--- src/decorator/constraint.decorator.ts
+++ src/decorator/constraint.decorator.ts
@@ -2,10 +2,10 @@ import { Validator } from '../validator/';
 import { ValidationMetadata } from './';
 import { validationFactory } from '../';
 
-export function Constraint(validator: Validator): Function {
+export function Constraint(...validators: Validator[]): Function {
   return function isDecorator(target: any, propName: string): void {
     const meta = new ValidationMetadata(
-      target.constructor, propName, validator);
+      target.constructor, propName, validators);
     
     validationFactory.addMetadata(meta);
   }
