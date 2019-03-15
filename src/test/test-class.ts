05b9142effa8bb06dced7935c12374fc63636458
Ben Botto <benjamin.botto@gmail.com>
2019-03-15 08:40:51 -0700

    Removes constraints (cleared and added during tests).

--- src/test/test-class.ts
+++ src/test/test-class.ts
@@ -1,16 +1,11 @@
-import { Constraint } from '../';
-import { StringValidator, MaxLengthValidator, PhoneValidator } from '../';
-
 /**
  * This class is only used for testing and is not part of the build.
  */
 export class TestClass {
-  @Constraint(new StringValidator())
-  @Constraint(new MaxLengthValidator(10))
+  id: number;
   firstName: string;
-
-  @Constraint(new StringValidator())
-  @Constraint(new PhoneValidator())
   phone: string;
+  email: string;
+  dob: Date;
 }
 
